d6dcce5dc6e7287c771c25d41e13983e52c65c3f
Raniel Araujo <araujoraniel07@gmail.com>
2022-05-10 22:26:08 -0300

    atualizando codigo commit

--- assets/JS/appPortifolio.js
+++ assets/JS/appPortifolio.js
@@ -2,6 +2,10 @@ const menuMobile = document.querySelector('.list-menu');
 const btnMenu = document.querySelector('#btn-menu');
 const imgs = document.getElementById("img");
 const img = document.querySelectorAll("#img img");
+const menuBar = document.getElementById("menu-bar");
+const btnNoturno = document.querySelector('.modo-tela');
+const portifolio = document.querySelector('.portifolio-rasa');
+
 
 function Menu() {
     this.inicio = () => {
@@ -23,7 +27,25 @@ function carousel() {
 
 }
 
+function Noturno(){
+
+    this.clickBtn = () => {btnNoturno.addEventListener('click', e =>{
+        menuBar.classList.toggle('noturno');
+        portifolio.classList.toggle('noturno');
+        btnNoturno.classList.toggle('noturno');
+        btnMenu.classList.toggle('noturno');
+
+    })
+
+    }
+
+    this.modoNoturno = () => {
+        this.clickBtn();
+    }
+}
+
 setInterval(carousel, 1800);
+const noturno = new Noturno();
 const menu = new Menu();
 menu.inicio();
-
+noturno.modoNoturno();
